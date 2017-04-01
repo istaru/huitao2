574fd0c6e8703e60c25d457b16b947e31bfa2abf
root1 <root1@knight0zh.local>
2017-04-01 18:01:58 +0800

    4/1

---
+++ Applications/Controllers/behaviourTempController.php
@@ -0,0 +1,16 @@
+<?php
+class BehaviourTempController extends AppController
+{
+    public $status  = true;
+    public $count   = 10;   //记录商品数量
+
+
+    //{"user_id":"","num_iid"}
+    /**
+     * [click 用户点击]
+     */
+    public function click()
+    {
+
+    }
+}
